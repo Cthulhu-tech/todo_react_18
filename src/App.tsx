72a48a2ec397d7199c661d9efcb5ea2f7ef11e5b
l lThrackerzod <mentsoff.daniar@yandex.ru>
2022-09-14 02:30:33 +0400

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom"
+import { Route, Routes } from "react-router"
+
+export const App = () => {
+
+return  <BrowserRouter>
+<Routes>
+  <Route path="/" element={<></>}>
+
+    </Route>
+</Routes>
+</BrowserRouter>
+
+}
