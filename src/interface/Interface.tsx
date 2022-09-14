c73e35c0dd430beb5f5d9b83020a36d524ddaaae
l lThrackerzod <mentsoff.daniar@yandex.ru>
2022-09-14 05:35:39 +0400

    interface type update

--- src/interface/Interface.tsx
+++ src/interface/Interface.tsx
@@ -1,10 +1,26 @@
 export interface IAction <T, U> {
-    readonly type: T
-    readonly payload: U
+    readonly type: T;
+    readonly payload: U;
 }
 
 export type UserPayload = {
-    id: null;
-    user: null;
-    token: null;
+    id: null | number;
+    user: null | string;
+    token: null | string;
+    load: boolean;
+}
+
+export interface IUseFetch <T> {
+    readonly load: boolean;
+    readonly data: T | null;
+    readonly error: ErrorData | null;
+    fetchData: (url: string) => void;
+}
+
+export type ErrorData = {
+    message: string;
+}
+
+export type StoreData = {
+    UserDataStore: UserPayload
 }
