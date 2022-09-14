6765e862b1a5b26e522637dc2c5ccefa9790d65f
l lThrackerzod <mentsoff.daniar@yandex.ru>
2022-09-14 05:34:48 +0400

    useFetch logic end

---
+++ src/hook/useFetch.tsx
@@ -0,0 +1,29 @@
+import { ErrorData, IUseFetch } from "../interface/Interface"
+import { useState } from "react"
+
+export function useFetch <T = any>(method: string = 'POST'): IUseFetch<T>{
+
+    const [data, setData] = useState<T | null>(null)
+    const [load, setLoad] = useState(true)
+    const [error, setError] = useState<ErrorData | null>(null)
+
+    const fetchData = (url: string) => {
+
+        fetch(url, {
+            method,
+        })
+        .then(response => response.json())
+        .then((json: T) => {
+            setData(json)
+            setLoad(false)
+        })
+        .catch(err => {
+
+            setLoad(false)
+        })
+
+    }
+
+    return {load, data, error, fetchData}
+
+}
